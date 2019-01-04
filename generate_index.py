67282ef7a5aed224cf221f3a840432ccd653fa0f
MaximLyubin <46385775+MaximLyubin@users.noreply.github.com>
2019-01-05 01:58:29 +0300

    Created page about.html and changed the main

--- generate_index.py
+++ generate_index.py
@@ -1,30 +1,52 @@
 # coding: utf-8
-from horoscope import generate_prophecies 
+from horoscope import generate_prophecies
 from datetime import datetime as dt
 
 def generate_page (head, body):
-	page = f"<html>{head}{body}</html>"
-	return page
+    page = f"<html>{head}{body}</html>"
+    return page
 
 def generate_head (title):
-	head = f"""<head>
-	<meta charset=utf-8> 
+    head = f"""<head>
+	<meta charset=utf-8>
 	<title> {title}</title>
 	</head>"""
-	return head 
+    return head
 
 def generate_body (header, paragraphs):
-	body = f"<h1>{header}</h1>"
-	for p in paragraphs:
-		body = body + f"<p>{p}</p>"
-	return f"<body>{body}</body>"
+    body = f"<h1>{header}</h1>"
+    for p in paragraphs:
+        body = body + f"<p>{p}</p>"
+    
+    body = body + f"<hr><a href=\"about.html\">О реализации</a>"
+    return f"<body>{body}</body>"
 
-def save_page (title, header, paragraphs, output = "index.html"):
-	fp = open (output, "w")
-	today = dt.now().date()
-	page = generate_page (head=generate_head(title), body = generate_body(header=header, paragraphs=paragraphs))
-	print(page, file=fp)
-	fp.close()
+def generate_about_page_body(header, paragraphs,picture):
+    body = f"<h1>{header}</h1>"
+    body = body + f"<img src=\"{picture}\" width=\"100\">"
+    body = body +"<ol><li>Времена дня:</li><ul><li>Утром</li><li>Вечером</li><li>После обеда</li></ul><li>Глаголы:</li><ul><li>остерегайтесь</li><li>ожидайте</li></ul></ol>"
+    body = body + f"<a href=\"index.html\">Главная</a>"
+    return f"<body>{body}</body>"
+
+def save_page (title, header, paragraphs, output):
+    fp = open (output, "w")
+    today = dt.now().date()
+    page = generate_page (head=generate_head(title), body = generate_body(header=header, paragraphs=paragraphs))
+    print(page, file=fp)
+    fp.close()
+
+def save_about_page (title, header, paragraphs, picture, output):
+    fp = open (output, "w")
+    today = dt.now().date()
+    page = generate_page (head=generate_head(title), body = generate_about_page_body(header=header, paragraphs=paragraphs, picture=picture))
+    print(page, file=fp)
+    fp.close()
+
+   
 
 today = dt.now().date()
-save_page (title="Гороскоп на сегодня", header="Что день " + str(today) + " готовит", paragraphs=generate_prophecies(),)
+save_page (title="Гороскоп на сегодня", header="Ваши пресказания на " + str(today), paragraphs=generate_prophecies(),output = "index.html")
+
+save_about_page(title="О реализации",header="О чем все это",paragraphs= "", picture = "horoscope_pic.jpg", output = "about.html")
+
+
